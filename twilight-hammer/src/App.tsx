9fce431688649cfb4fb5033ecaff76332ae2e98e
Weiran Wu <weiranwu@microsoft.com>
2024-07-08 16:59:37 +0800

    Add tranlation examples in client

--- twilight-hammer/src/App.tsx
+++ twilight-hammer/src/App.tsx
@@ -5,13 +5,16 @@ import edgeLogo from "./assets/edge.png";
 import "./App.css";
 import {
   Button,
+  Caption1,
   Input,
   makeStyles,
   Subtitle1,
+  Title1,
   tokens,
 } from "@fluentui/react-components";
 import { call } from "./containers/api";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles({
   bar: {
@@ -28,6 +31,7 @@ const useStyles = makeStyles({
 });
 function App() {
   const styles = useStyles();
+  const { t } = useTranslation();
   const [toSend, SetToSend] = useState<string>("Hello Twilight Hammer!");
   const [received, SetReceived] = useState<string>("");
   return (
@@ -39,14 +43,20 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://developer.microsoft.com/en-us/microsoft-edge/webview2" target="_blank">
+        <a
+          href="https://developer.microsoft.com/en-us/microsoft-edge/webview2"
+          target="_blank"
+        >
           <img src={edgeLogo} className="logo react" alt="WebView2 logo" />
         </a>
-        <a href="https://learn.microsoft.com/en-us/cpp/windows/overview-of-windows-programming-in-cpp" target="_blank">
+        <a
+          href="https://learn.microsoft.com/en-us/cpp/windows/overview-of-windows-programming-in-cpp"
+          target="_blank"
+        >
           <img src={cppLogo} className="logo react" alt="C++ logo" />
         </a>
       </div>
-      <h1>Vite + React + WebView2 + C++</h1>
+      <Title1>Vite + React + WebView2 + C++</Title1>
       <div className={styles.card}>
         <Input
           value={toSend}
@@ -61,13 +71,14 @@ function App() {
             )
           }
         >
-          Send the above message to host
+          {t("send to host")}
         </Button>
-        <Subtitle1>Received: {received}</Subtitle1>
+        <Subtitle1>
+          {t("received")}
+          {received}
+        </Subtitle1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Caption1>{t("learn more")}</Caption1>
     </>
   );
 }
---
+++ twilight-hammer/src/i18n/config.ts
@@ -0,0 +1,20 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslation from "./en/translation.json";
+import zhTranslation from "./zh/translation.json";
+
+void i18next.use(initReactI18next).init({
+  lng: "en", // if you're using a language detector, do not define the lng option
+  debug: import.meta.env.DEV,
+  resources: {
+    en: {
+      translation: enTranslation,
+    },
+    zh: {
+      translation: zhTranslation,
+    },
+  },
+  // if you see an error like: "Argument of type 'DefaultTFuncReturn' is not assignable to parameter of type xyz"
+  // set returnNull to false (and also in the i18next.d.ts options)
+  // returnNull: false,
+});
